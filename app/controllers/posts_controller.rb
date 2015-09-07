325495b8b53845dd283a3594158a0914e147aab8
prashantgupta24 <prashantgupta24@gmail.com>
2015-09-06 23:48:18 -0400

    Changes for CRUD of sample app in Rails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,28 @@
 class PostsController < ApplicationController
 	def index
-		@content_first = 'testing one two three'
+		@posts = Post.all
 	end
 	def new
+		@post = Post.new
 	end
 	def create
+		@post = Post.new message_params
+		if @post.save
+			redirect_to posts_path
+		else
+			render "new"
+		end
 	end
 	def edit
 	end
 	def update
 	end
 	def show
+		@post = Post.find(params[:id])
 	end
 	def destroy
 	end
+	def message_params
+      params.require(:post).permit(:title, :body, :category_id)
+   end
 end
