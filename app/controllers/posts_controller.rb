7861f68631b53b9d74acd8d464d850835f1d3c86
prashantgupta24 <prashantgupta24@gmail.com>
2015-09-06 21:58:46 -0400

    My first sample Ruby on Rails app

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'testing one two three'
+	end
+	def new
+	end
+	def create
+	end
+	def edit
+	end
+	def update
+	end
+	def show
+	end
+	def destroy
+	end
+end
